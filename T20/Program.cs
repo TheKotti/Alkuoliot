7a5aad4f9e9f15a06f0a80d099e09306332c768d
TheKotti <kotti_fin@hotmail.com>
2017-01-15 20:45:36 +0200

    T20

--- T20/Program.cs
+++ T20/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Threading;
 
@@ -12,6 +8,97 @@ namespace T20
     {
         static void Main(string[] args)
         {
+
+            Random rndTime = new Random();
+            int pauseTime = rndTime.Next(2500, 9001);
+            ConsoleKeyInfo input;
+            bool written = false, written2 = false;
+            Stopwatch kello = new Stopwatch();
+            Stopwatch endTimer = new Stopwatch();
+            Thread.Sleep(1);
+
+            Console.WriteLine("Kahden pelaajan reaktiotesti");
+            Console.WriteLine();
+            Console.WriteLine("Paina näppäintä sanan 'NYT' ilmestyessä");
+            Console.WriteLine("Pelaaja Q, näppäimesi on Q");
+            Console.WriteLine("Pelaaja P, näppäimesi on P");
+            Console.WriteLine();
+            Console.WriteLine("Paina välilyöntiä aloittaaksesi, jotain muuta lopettaaksesi");
+            Console.WriteLine();
+
+            
+            input = Console.ReadKey(true);
+            if (Convert.ToString((input.Key)) == "Spacebar")
+            {
+                kello.Start();
+
+                //käsittelee aikaset painallukset
+                while (pauseTime > kello.ElapsedMilliseconds)
+                {
+                    if (Console.KeyAvailable == true)
+                    {
+                        input = Console.ReadKey(true);
+
+                        if (Convert.ToString((input.Key)) == "Q" && endTimer.ElapsedMilliseconds == 0)
+                        {
+                            Console.WriteLine("Pelaaja P voitti!");
+                            Console.WriteLine("Pelaaja Q painoi nappia {0}ms liian aikaisin.", (pauseTime - kello.ElapsedMilliseconds));
+                            Console.WriteLine();
+                            endTimer.Start();
+                        }
+                        else if (Convert.ToString((input.Key)) == "P" && endTimer.ElapsedMilliseconds == 0)
+                        {
+                            Console.WriteLine("Pelaaja Q voitti!");
+                            Console.WriteLine("Pelaaja P painoi nappia {0}ms liian aikaisin.", (pauseTime - kello.ElapsedMilliseconds));
+                            Console.WriteLine();
+                            endTimer.Start();
+                        }
+                    }
+                }
+
+                //printtaa NYT
+                while (written == false && endTimer.ElapsedMilliseconds == 0)
+                {
+                    if (pauseTime <= kello.ElapsedMilliseconds && written == false)
+                    {
+                        Console.WriteLine("NYT");
+                        Console.WriteLine();
+                        written = true;
+                    }
+                }
+
+
+                //käsittelee myöhäiset painallukset
+                while (endTimer.ElapsedMilliseconds == 0)
+                {
+                    if (Console.KeyAvailable == false)
+                    {
+                        input = Console.ReadKey(true);
+
+                        if (Convert.ToString((input.Key)) == "Q" && endTimer.ElapsedMilliseconds == 0)
+                        {
+                            Console.WriteLine("Pelaaja Q voitti!");
+                            Console.WriteLine("Reaktioaika: " + (kello.ElapsedMilliseconds - pauseTime) + "ms");
+                            Console.WriteLine();
+                            endTimer.Start();
+                        }
+                        else if (Convert.ToString((input.Key)) == "P" && endTimer.ElapsedMilliseconds == 0)
+                        {
+                            Console.WriteLine("Pelaaja P voitti!");
+                            Console.WriteLine("Reaktioaika: " + (kello.ElapsedMilliseconds - pauseTime) + "ms");
+                            Console.WriteLine();
+                            endTimer.Start();
+                        }
+                    }
+                    while (true)
+                    {
+                        if (endTimer.ElapsedMilliseconds > 1000)
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+            } 
         }
     }
 }
