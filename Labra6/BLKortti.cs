6a15f35033653626897e561529f8ef2b173c4888
K8721 <K8721@ICT-421-WS-15>
2017-01-25 15:39:07 +0200

    Korttipakka

---
+++ Labra6/BLKortti.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Card
+    {
+        public string House { get; set; }
+        public int Number { get; set; }
+
+        public Card()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Number + " of " + House;
+        }
+    }
+
+    //PAKKA
+    class Deck
+    {
+        public Card[] Cards;
+        private Random _random = new Random();
+
+        public Deck()
+        {
+            Cards = new Card[52];
+            int i = 0;
+            for (i = 0; i < 13; i++)
+            {
+                Cards[i] = new Card() { House = "Spades", Number = i + 1 };
+            }
+            for (i = 0; i < 13; i++)
+            {
+                Cards[i+13] = new Card() { House = "Hearts", Number = i + 1 };
+            }
+            for (i = 0; i < 13; i++)
+            {
+                Cards[i+26] = new Card() { House = "Clubs", Number = i + 1 };
+            }
+            for (i = 0; i < 13; i++)
+            {
+                Cards[i+39] = new Card() { House = "Diamonds", Number = i + 1 };
+            }
+        }
+        public void Shuffle()
+        {
+            int n = Cards.Length-1;
+            for (int i = 0; i < n; i++)
+            {
+                int r = i + (int)(_random.NextDouble() * (n - i));
+                Card t = Cards[r];
+                Cards[r] = Cards[i];
+                Cards[i] = t;
+            }
+        }
+    }
+}
---
+++ Labra6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace JAMK.IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestaaHloRekisteri();
+            TestaaKortit();
+        }
+
+        static void TestaaHloRekisteri()
+        {
+            //luodaan muutama testihenkilö
+            Henkilot poppoo = new Henkilot();
+            Henkilo hlo = new Henkilo { Etunimi = "Jack", Sukunimi = "Russell", Hetu = "123123-321A" };
+            Henkilo hlo1 = new Henkilo { Etunimi = "Qewr", Sukunimi = "qwerqwer", Hetu = "5432-33451A" };
+            Henkilo hlo2 = new Henkilo { Etunimi = "Asdf", Sukunimi = "asdfsadf", Hetu = "765456-86454G" };
+            Henkilo hlo3 = new Henkilo { Etunimi = "iuyt", Sukunimi = "iuytuyi", Hetu = "111" };
+
+            //lisätään henkilot poppooseen
+            poppoo.LisaaHenkilo(hlo);
+            poppoo.LisaaHenkilo(hlo1);
+            poppoo.LisaaHenkilo(hlo2);
+            poppoo.LisaaHenkilo(hlo3);
+
+            //tulostetaan poppoo
+            foreach (var item in poppoo.HenkiloLista)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            //thedänn hakuja
+            Console.WriteLine(poppoo.HaeHenkilo(0));
+            Console.WriteLine("Hae hetulla");
+            Console.WriteLine(poppoo.HaeHenkiloHetulla(Console.ReadLine()));
+        }
+        static void TestaaKortit()
+        {
+            Deck pakka = new IT.Deck();
+
+            foreach (var item in pakka.Cards)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            
+            pakka.Shuffle();
+            foreach (var item in pakka.Cards)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
