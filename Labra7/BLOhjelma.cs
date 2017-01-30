66c53be9b9c6d03d0c5b871b3bcdcbe17e6021d2
TheKotti <kotti_fin@hotmail.com>
2017-01-30 20:21:01 +0200

    telkkariohjelmasdnat

---
+++ Labra7/BLOhjelma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    [Serializable]
+    class Ohjelma
+    {
+        public string  Nimi { get; set; }
+        public int Kanava { get; set; }
+        public string Alkuaika { get; set; }
+        public string Loppuaika { get; set; }
+        public string Info { get; set; }
+
+        public Ohjelma()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Nimi + " " + Kanava + " " + Alkuaika + " " + Loppuaika + " " + Info;
+        }
+    }
+}
--- Labra7/Program.cs
+++ Labra7/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using JAMK.IT;
+
 
 namespace JAMK.IT
 {
@@ -11,7 +15,7 @@ namespace JAMK.IT
     {
         static void Main(string[] args)
         {
-            Teht3();
+            Teht4();
         }
 
         static void Teht1()
@@ -64,8 +68,8 @@ namespace JAMK.IT
         {
             try
             {
-                List<int> intit = new List<int>();
-                List<float> floatit = new List<float>();
+                //List<int> intit = new List<int>();
+                //List<float> floatit = new List<float>();
                 int conversion = 0;
                 float conversion2 = 0.0F;
                 string path = @"d:\K8721\ConsoleApplication2\Labra7", temp;
@@ -88,10 +92,56 @@ namespace JAMK.IT
                         outputFile.WriteLine(conversion2);
                         outputFile.Close();
                     }
-                } while (Single.TryParse(temp, out conversion2) || Int32.TryParse(temp, out conversion));
+                    //outputFile.Close(); menee tähän ja pois noista muista
+                } while (float.TryParse(temp, out conversion2) || int.TryParse(temp, out conversion));
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Teht4()
+        {
+            try
+            {
+                int valinta;
+                List<Ohjelma> ohjelmat = new List<Ohjelma>();
+                IFormatter formatter = new BinaryFormatter();
+
+                Console.WriteLine("(1) Luo tv-ohjelmat pääohjelmalla");
+                Console.WriteLine("(2) Hae tv-ohjelmat tiedostosta");
+                Console.WriteLine("(Muu) lopeta");
+                int.TryParse(Console.ReadLine(), out valinta);
+                if (valinta == 1)
+                {
+                    ohjelmat.Add(new Ohjelma { Nimi = "Band of Brothers", Kanava = 3, Alkuaika = "22:00", Loppuaika = "23:00", Info = "Tässä soditaan" });
+                    ohjelmat.Add(new Ohjelma { Nimi = "Stargate", Kanava = 8, Alkuaika = "21:00", Loppuaika = "21:55", Info = "Tässä soditaan avaruudessa" });
+                    ohjelmat.Add(new Ohjelma { Nimi = "Twin Peaks", Kanava = 1, Alkuaika = "12:00", Loppuaika = "15:00", Info = "Mitäköhän hittoa" });
+                    ohjelmat.Add(new Ohjelma { Nimi = "Lost", Kanava = 6, Alkuaika = "19:00", Loppuaika = "20:00", Info = "Tämä on tosi huono" });
+
+                    for (int i = 0; i < ohjelmat.Count; i++)
+                    {
+                        Console.WriteLine(ohjelmat[i].ToString());
+                    }
+
+                    Stream tallennaStream = new FileStream("ohjelmat.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                    formatter.Serialize(tallennaStream, ohjelmat);
+                    tallennaStream.Close();
+                }
+                else if (valinta == 2)
+                {
+                    Stream avaaStream = new FileStream("ohjelmat.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                    ohjelmat = (List<Ohjelma>)formatter.Deserialize(avaaStream);
+                    for (int i = 0; i < ohjelmat.Count; i++)
+                    {
+                        Console.WriteLine(ohjelmat[i].ToString());
+                    }
+                    avaaStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
                 Console.WriteLine(ex.Message);
             }
         }
